1931dd7b122c23add7429df2823b7dd0f80329ee
erenkucukk <erenkucukk53@gmail.com>
2025-09-11 11:41:38 +0300

    notification image

--- src/app/pages/permission/notification-bell.component.ts
+++ src/app/pages/permission/notification-bell.component.ts
@@ -6,10 +6,16 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   standalone: true,
     imports: [NgIf],
   template: `
-    <div class="notification-bell" (click)="bellClick.emit()">
-      <span class="bell-icon">&#128276;</span>
-      <span class="notification-count" *ngIf="count > 0">{{ count }}</span>
-    </div>
+<div class="notification-bell" (click)="bellClick.emit()">
+  <span class="bell-icon">
+    <!-- Modern SVG Çan İkonu -->
+    <svg width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#ff6600" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M18 8a6 6 0 0 0-12 0c0 7-3 9-3 9h18s-3-2-3-9"></path>
+      <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+    </svg>
+  </span>
+  <span class="notification-count" *ngIf="count > 0">{{ count }}</span>
+</div>
   `,
   styles: [`
     .notification-bell {
