e19e59bb14675e965971bbb38c3efb846ce25ea6
Doğukan SOLAK <104324604+dogukaansolak@users.noreply.github.com>
2025-09-11 12:41:25 +0300

    Analiz ve Rapor ön kısmı

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app';
 import { importProvidersFrom } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
@@ -10,11 +10,11 @@ import { routes } from './app/app.routes'; // Eğer routing tanımlıysa
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(
-      BrowserModule,
+      BrowserAnimationsModule,  // BrowserModule yerine animations module
       FormsModule,
       ReactiveFormsModule,
       HttpClientModule,
-      RouterModule.forRoot(routes) // Eğer route kullanıyorsan
+      RouterModule.forRoot(routes) // Eğer routing kullanıyorsan
     )
   ]
 }).catch(err => console.error(err));
