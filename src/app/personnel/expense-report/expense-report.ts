8e912c6ad77a149ce8109a9a0b0776e1ba4d2dce
Doğukan SOLAK <104324604+dogukaansolak@users.noreply.github.com>
2025-09-11 11:41:04 +0300

    personnel sayfasında html css ayarlandı

--- src/app/personnel/expense-report/expense-report.ts
+++ src/app/personnel/expense-report/expense-report.ts
@@ -10,9 +10,27 @@ import { FormsModule } from '@angular/forms';
 })
 export class ExpenseReportComponent {
   expense = { date: '', amount: 0, note: '' };
+  file: File | null = null;
+
   submitExpense() {
-    console.log('Gider gönderildi:', this.expense);
+    console.log('Gider gönderildi:', this.expense, 'Dosya:', this.file);
     alert('Gider gönderildi!');
     this.expense = { date: '', amount: 0, note: '' };
+    this.file = null;
+  }
+
+  onFileSelected(event: any) {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      if (
+        selectedFile.type.startsWith('image/') || 
+        selectedFile.type === 'application/pdf'
+      ) {
+        this.file = selectedFile;
+      } else {
+        alert('Sadece resim ve PDF dosyaları yükleyebilirsiniz.');
+        event.target.value = '';
+      }
+    }
   }
 }
