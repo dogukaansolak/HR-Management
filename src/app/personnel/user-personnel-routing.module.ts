e92c0c3d187944986a1d016b4651a5067b429262
guleryolcu <guleryolc@hotmail.com>
2025-09-12 15:54:41 +0300

    destek sayfası personel paneline de eklendi. Buton rengi değiştirildi.

--- src/app/personnel/user-personnel-routing.module.ts
+++ src/app/personnel/user-personnel-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PersonnelGuard } from '../guards/personnel.guard';
-
-// 1. Tüm component'lerinizi import edin.
-// "Home" component'i, ana panele girildiğinde gösterilecek varsayılan sayfadır.
-// Eğer böyle bir component'iniz yoksa oluşturmanız veya varsayılan olarak
-// başka bir sayfayı (örn. LeaveRequestComponent) göstermeniz gerekir.
-//import { DashboardPersonnelHomeComponent } from '../personnel/dashboard-personnel-home/dashboard-personnel-home'; 
 import { DashboardPersonnelComponent } from '../personnel/dashboard-personnel/dashboard-personnel';
 import { LeaveRequestComponent } from './leave-request/leave-request';
 import { ExpenseReportComponent } from './expense-report/expense-report';
 import { PersonnelSettingsComponent } from './personnel-settings/personnel-settings';
+import { SupportComponent } from '../support/support.component';
 
 
 const routes: Routes = [
@@ -33,6 +28,10 @@ const routes: Routes = [
       { 
         path: 'expense-report', // /personnel-panel/expense-report
         component: ExpenseReportComponent 
+      },
+      { 
+        path: 'support', // /dashboard/support
+        component: SupportComponent 
       },
       { 
         path: 'settings',       // /personnel-panel/settings
